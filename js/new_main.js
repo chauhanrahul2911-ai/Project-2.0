0f0462496e36d87dad07f79f88bc9c91a5646171
chauhanrahul2911-ai <chauhanrahul2911@gmail.com>
2026-07-15 21:11:11 +0530

    Update new_main.js

--- js/new_main.js
+++ js/new_main.js
@@ -409,6 +409,7 @@ function isBackForwardNavigation(event) {
 }
 // 🌐 Case 1: Jab page bilkul pehli baar normal load/refresh ho
 window.onload = function(event) {
+    initDashboard();
     history.replaceState(
     { activeScreen: "screen-subjects" }, "");
 };
